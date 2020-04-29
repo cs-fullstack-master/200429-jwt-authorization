569570ecbe5445bdb42f0a6d4b8980c968d43e02
kevin-codecrew <kevin@code-crew.org>
2020-04-29 08:24:08 -0500

    init

---
+++ server/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'YOUR_DB_CNXN_STR',
+    secretKey: "t0pS3cr3tK3y",
+    listenPort: 8000
+};
---
+++ server/routes/api.js
@@ -0,0 +1,12 @@
+// import express
+let express = require('express');
+// create router
+let router = express.Router();
+// json middleware
+router.use(express.json());
+
+router.get('/', (req,res) => {
+    res.send(`<h1>Success!</h1>`);
+});
+
+module.exports = router;
